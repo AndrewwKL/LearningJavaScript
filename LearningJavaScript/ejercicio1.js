b245a5c770dcf01159db62808cbe27d1a65a1342
AndrewwKL <andresr.cacereso@konradlorenz.edu.co>
2024-09-22 12:59:21 -0500

    Initial commit of my first JavaScript exercise

---
+++ LearningJavaScript/ejercicio1.js
@@ -0,0 +1,31 @@
+let myName="Andres";
+let mySurname="Caceres";
+
+let age=20;
+
+let amIstudent=true;
+let amIaDoctor=false;
+
+let grade=null;
+
+let undefined;
+
+let addition= age+age;
+
+let subtraction=age-5;
+
+let comparisonOne = (age == 20);
+let comparisonTwo = (age === '20');
+
+console.log ("Name: ", myName);
+console.log("SurName: ", mySurname);
+console.log("Age", age);
+console.log("Sudent: ", amIstudent);
+console.log( "Doctor: " + amIaDoctor + "\n" +
+    "age + age: " + addition);
+console.log ("age-5: " + subtraction + "\n" + 
+    "Is the age equal to 20? : " + comparisonOne + "\n" +
+    "Is the age equal to '20'? : " + comparisonTwo
+)
+
+
