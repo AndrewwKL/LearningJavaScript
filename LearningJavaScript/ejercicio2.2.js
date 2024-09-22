3ccbfb783c662da3864a48135b9735d47f114fe8
AndrewwKL <andresr.cacereso@konradlorenz.edu.co>
2024-09-22 14:02:47 -0500

    Second JavaScript exercise

---
+++ LearningJavaScript/ejercicio2.2.js
@@ -0,0 +1,1 @@
+let garde=20;
---
+++ LearningJavaScript/ejercicio2.js
@@ -0,0 +1,29 @@
+// Conditionals
+let temperature = 30;
+
+if (temperature > 30) {
+    console.log("It's hot.");
+} else if (temperature < 15) {
+    console.log("It's cold.");
+} else {
+    console.log("The weather is nice.");
+}
+
+// For loop
+for (let i = 1; i <= 5; i++) {
+    console.log("Counting with for:", i);
+}
+
+// While loop
+let j = 1;
+while (j <= 5) {
+    console.log("Counting with while:", j);
+    j++;
+}
+
+// Do...while loop
+let k = 1;
+do {
+    console.log("Counting with do...while:", k);
+    k++;
+} while (k <= 5);
